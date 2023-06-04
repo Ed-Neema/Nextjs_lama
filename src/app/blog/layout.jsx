390d0406beb616acd609b914a909e853a6295d29
Ed-Neema <98660147+Ed-Neema@users.noreply.github.com>
2023-06-04 06:53:02 +0300

    routing subdirectories

---
+++ src/app/blog/layout.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const BlogLayout = ({ children }) => {
+  return (
+    <>
+    This is the blog layout
+      <div>{children}</div>
+    </>
+  );
+};
+
+export default BlogLayout;
